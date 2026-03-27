902b49bdd4f85287ac8ceec940b558e0c2287566
Aveiro11 <aveirowahid@gmail.com>
2026-03-27 13:44:09 +0100

    env added and gitignore modification

---
+++ src/azure.ts
@@ -0,0 +1,7 @@
+export const SPEECH_KEY = import.meta.env.VITE_SPEECH_KEY;
+export const SPEECH_REGION = import.meta.env.VITE_SPEECH_REGION;
+export const LANG_KEY = import.meta.env.VITE_LANG_KEY;
+export const LANG_ENDPOINT = import.meta.env.VITE_LANG_ENDPOINT;
+export const LANG_PROJECT = import.meta.env.VITE_LANG_PROJECT;
+export const LANG_DEPLOYMENT = import.meta.env.VITE_LANG_DEPLOYMENT;
+export const CUSTOM_SPEECH_ENDPOINT_ID = import.meta.env.VITE_CUSTOM_SPEECH_ID;
